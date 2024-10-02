b19dae3383633b1ee9d21a35bd74ee3f2153918f
javid khanushiya <javidkhanushiya21@gmail.com>
2024-10-02 14:00:14 +0530

    all game

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+let youscore = 0;
+let compscore = 0;
+ 
+let choeses = document.querySelectorAll(".couse");
+
+// console.log(choeses);
+
+
+choeses.forEach((chois) => {
+chois.addEventListener("click" , () => {
+    console.log("click");
+    
+})
+})
